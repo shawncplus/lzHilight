b99fc1d52f85d47e10497b7ea4043d12cfc47368
Shawn Biddle <shawncplus@gmail.com>
2009-12-07 11:14:32 -0500

    Updated the install script to A) not suck, B) not be destructive to the git checkout

--- install.sh
+++ install.sh
@@ -1,15 +1,51 @@
 #!/bin/bash
 
-echo "Installing man page..."
-mv ./docs/lzhighlight.1.gz /usr/share/man/man1/
-echo "Configuring highlight script..."
-sed -i "s#<INSTALL_RESC_DIR>#`pwd`/#" highlight
-echo "Creating symlink"
-mv ./highlight /usr/bin/lzhighlight
+manpathdefault="/usr/share/man/man1/"
+lmanpath="$manpathdefault"
+echo "Install man page for all users? [Yn]"
+read -s -n 1 answer
+case "$answer" in
+[nN])
+	echo -n "Man page path: [$manpathdefault] "
+	read lmanpath
+	if [ -z "$lmanpath" ]
+	then
+		lmanpath="$manpathdefault"
+	fi
+;;
+esac
 
-echo "Install finished, would you like to view the man page? [Y/n]";
-read answer
+cp ./docs/lzhighlight.1.gz $lmanpath
+
+default="/usr/bin/lzhighlight"
+installpath="$default"
+echo "Install for all users? [Yn]"
+read -s -n 1 answer
+case "$answer" in
+[nN])
+	echo -n "Install path: [$default] "
+	read installpath
+	if [ -z "$installpath" ]
+	then
+		installpath="$installpathdefault"
+	fi
+;;
+esac
+
+cp ./highlight $installpath
+echo "Installing and configuring highlight script..."
+sed -i "s#<INSTALL_RESC_DIR>#`pwd`/#" $installpath
+
+echo "Install finished, would you like to view the man page? [Yn]";
+read -s -n 1 answer
+case "$answer" in
+[nN]) ;;
+*)    man lzhighlight;;
+esac
+
+echo "Run test? [Yn]"
+read -s -n 1 answer
 case "$answer" in
 [nN]) exit;;
-*)    man lzhighlight;;
+*)    $installpath tests/fulltest.php;;
 esac
