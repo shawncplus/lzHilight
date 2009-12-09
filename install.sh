37e26abc7d350162dbc5d81a16fe00e34cc4887b
Shawn Biddle <shawncplus@gmail.com>
2009-12-09 17:16:26 -0500

    Updated the install script to set a default theme

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ case "$answer" in
 ;;
 esac
 
-cp ./docs/lzhighlight.1.gz $lmanpath
+cp docs/lzhighlight.1.gz ${lmanpath}lzhighlight.1.gz
 
 default="/usr/bin/lzhighlight"
 installpath="$default"
@@ -32,8 +32,33 @@ case "$answer" in
 ;;
 esac
 
+if [ -e $installpath ]
+then
+	rm $installpath
+fi
 ln -s `pwd`/highlight $installpath
+
 echo "Installing and configuring highlight script..."
+
+echo "Pick your default theme:"
+for dir in themes/*
+do
+	echo -n "	"
+	basename $dir
+done
+echo -n ":"
+read theme
+
+if [ ! -d themes/$theme ]
+then
+	echo -e "\e[31mWarning: You picked a non-existant theme as your default. You will have to manually symlink later...\e[0m"
+fi
+
+if [ -h syntax ]
+then
+	rm syntax
+fi
+ln -s themes/$theme syntax
 
 echo "Install finished, would you like to view the man page? [Yn]";
 read -s -n 1 answer
